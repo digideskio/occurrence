7aa753f53dead7a13d67dc9266791e8e55f69325
Federico Mendez <fmendez@gbif.org>
2016-05-10 12:28:11 +0200

    trying ignore with module

--- occurrence-elasticsearch/src/main/java/org/gbif/occurrence/es/index/mr/OccurrenceAvroMapper.java
+++ occurrence-elasticsearch/src/main/java/org/gbif/occurrence/es/index/mr/OccurrenceAvroMapper.java
@@ -10,7 +10,9 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import org.codehaus.jackson.Version;
 import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.Module;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
@@ -24,6 +26,22 @@ public class OccurrenceAvroMapper extends Mapper<AvroKey<Occurrence>, NullWritab
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
   static {
     OBJECT_MAPPER.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+    OBJECT_MAPPER.withModule(new Module() {
+      @Override
+      public String getModuleName() {
+        return "ignore schema";
+      }
+
+      @Override
+      public Version version() {
+        return Version.unknownVersion();
+      }
+
+      @Override
+      public void setupModule(SetupContext context) {
+        context.setMixInAnnotations(Occurrence.class,MixIn.class);
+      }
+    });
   }
 
   @Override
