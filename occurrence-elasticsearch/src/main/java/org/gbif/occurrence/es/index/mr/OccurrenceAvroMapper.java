b9a40332677d08042a65f4fa5d69bbc58c936ada
Federico Mendez <fmendez@gbif.org>
2016-05-10 13:57:20 +0200

    using gson instead of jackson

--- occurrence-elasticsearch/src/main/java/org/gbif/occurrence/es/index/mr/OccurrenceAvroMapper.java
+++ occurrence-elasticsearch/src/main/java/org/gbif/occurrence/es/index/mr/OccurrenceAvroMapper.java
@@ -3,50 +3,45 @@ package org.gbif.occurrence.es.index.mr;
 import org.gbif.occurrence.avro.model.Occurrence;
 
 import java.io.IOException;
+import java.lang.reflect.Modifier;
 
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.avro.Schema;
 import org.apache.avro.mapred.AvroKey;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import org.codehaus.jackson.Version;
-import org.codehaus.jackson.annotate.JsonIgnore;
-import org.codehaus.jackson.map.Module;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
-
 public class OccurrenceAvroMapper extends Mapper<AvroKey<Occurrence>, NullWritable, NullWritable,Text> {
 
+  public static class MyExclusionStrategy implements ExclusionStrategy {
+    private final Class<?> typeToSkip;
 
-  public abstract class MixIn {
-    @JsonIgnore abstract Schema getSchema(); // we don't need it!
+    private MyExclusionStrategy(Class<?> typeToSkip) {
+      this.typeToSkip = typeToSkip;
+    }
+
+    public boolean shouldSkipClass(Class<?> clazz) {
+      return (clazz == typeToSkip);
+    }
+
+    public boolean shouldSkipField(FieldAttributes f) {
+      return f.getDeclaredClass() == Schema.class;
+    }
   }
 
-  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-  static {
-    OBJECT_MAPPER.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
-    OBJECT_MAPPER.withModule(new Module() {
-      @Override
-      public String getModuleName() {
-        return "ignore schema";
-      }
+  private static Gson GSON = new GsonBuilder()
+                              .excludeFieldsWithModifiers(Modifier.STATIC)
+                              .addSerializationExclusionStrategy(new MyExclusionStrategy(Occurrence.class))
+                              .create();
 
-      @Override
-      public Version version() {
-        return Version.unknownVersion();
-      }
-
-      @Override
-      public void setupModule(SetupContext context) {
-        context.setMixInAnnotations(Occurrence.class,MixIn.class);
-      }
-    });
-  }
 
   @Override
   public void map(AvroKey<Occurrence> occurrenceAvro, NullWritable value, Context context) throws IOException, InterruptedException {
-    context.write(NullWritable.get(),new Text(OBJECT_MAPPER.writeValueAsString(occurrenceAvro.datum())));
+    context.write(NullWritable.get(),new Text(GSON.toJson(occurrenceAvro.datum())));
   }
 
 }
