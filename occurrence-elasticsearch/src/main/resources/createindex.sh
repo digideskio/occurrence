3b42df47fb675364130ae30b64ae9750a196104f
Federico Mendez <fmendez@gbif.org>
2016-05-13 15:52:35 +0200

    utility scrip added

--- occurrence-elasticsearch/src/main/resources/createindex.sh
+++ occurrence-elasticsearch/src/main/resources/createindex.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 curl -XDELETE 'http://uatsolr-vh.gbif.org:9200/occurrence/'
 curl -XPOST 'http://uatsolr-vh.gbif.org:9200/occurrence/' -d @type_mappings.json
+
+curl -XGET 'http://uatsolr-vh.gbif.org:9200/occurrence/occurrence/_search?q=country:us' -d '{
+    "size": 0,
+    "aggs" : {
+        "grades_count" : { "terms" : { "field" : "basis_of_record" } }
+    }
+}'                                                                                                                       l
